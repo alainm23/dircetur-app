4cfca381c4d11a480833160cfc1a189fd45686d3
Alain M <alain23@protonmail.com>
2019-12-13 12:46:23 -0500

    Initial commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {MenuController} from '@ionic/angular'; 
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor(
+    private menu:MenuController
+  ) {}
+  abrirmenu (){
+    this.menu.open()
+  }
+}
